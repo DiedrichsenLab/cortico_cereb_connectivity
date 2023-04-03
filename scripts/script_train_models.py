d848039054393423fda083c21cf0ef8c397bcdf3
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2023-04-03 16:23:50 -0400

    Update script_train_models.py

--- scripts/script_train_models.py
+++ scripts/script_train_models.py
@@ -35,7 +35,7 @@ def train_models(logalpha_list = [0, 2, 4, 6, 8, 10, 12],
                                     dataset=config["train_dataset"]) 
          # get the list of trained connectivity models and training summary
    T = dataset.get_participants()
-   config["subj_list"] = T.participant_id[80:]
+   config["subj_list"] = T.participant_id
    config, conn_list, df_tmp =rm.train_model(config)
 
 
@@ -95,13 +95,14 @@ def eval_models(logalpha_list = [0, 2, 4, 6, 8, 10, 12],
 if __name__ == "__main__":
    # train_models(train_ses = 'all',dataset = "HCP",type='Tseries',crossed=None)
    # avrg_model(train_data = "HCP",train_ses= "all")
-   ED=["MDTB","WMFS", "Nishimoto", "Demand", "Somatotopic", "IBC"]
+   # ED=["MDTB","WMFS", "Nishimoto", "Demand", "Somatotopic", "IBC"]
+   ED=["Demand", "Somatotopic", "IBC"]
    # ET=["CondHalf","CondHalf", "CondHalf", "CondHalf", "CondHalf", "CondHalf"]
    # eval_models(eval_dataset = ED, train_dataset="MDTB", train_ses="all",eval_id = 'Md')
    # eval_models(eval_dataset = ED, train_dataset="MDTB", train_ses="ses-s1",eval_id = 'Mds1')
    for ed in ED:
-      train_models(train_ses = 'all',dataset = ed,cerebellum='MNISym2')
-      avrg_model(train_data = ed,train_ses= "all",cerebellum='MNISym2')
+      train_models(train_ses = 'all',dataset = ed,cerebellum='MNISymC2')
+      # avrg_model(train_data = ed,train_ses= "all",cerebellum='MNISymC2')
    # eval_models(eval_dataset = ED, eval_type = ET,train_dataset="HCP", train_ses="all",eval_id = 'Hc')
    # eval_models(eval_dataset = ED, train_dataset="Demand", train_ses="all",eval_id = 'De')
    # eval_models(eval_dataset = ED, train_dataset="Nishimoto", train_ses="all",eval_id = 'Ni')
