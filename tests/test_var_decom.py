c3ef30ca4b7db92e64758813301d352b07ac9e82
ashahb7 <shahbazi.2000.ali@gmail.com>
2025-06-27 08:28:28 -0400

    Update test_var_decom.py

--- tests/test_var_decom.py
+++ tests/test_var_decom.py
@@ -35,23 +35,24 @@ def rel_var_decom(dataset_name='MDTB', logalpha=6):
     data = []
     sub_list = fdata.get_dataset_class(gl.base_dir, dataset=dataset_name).get_participants().participant_id
     for sub_id in sub_list:
+        print(f"load subject {sub_id} for dataset {dataset_name}")
         mname_base = f"{dataset_name}_all_Icosahedron1002_L2reghalf"
         mname = mname_base + f"/{mname_base}_A{logalpha}_{sub_id}"
 
         mo, _ = cio.load_model(model_base_path+mname)
 
         sub_data_list = []
-        sub_data_list.append(mo.coef_1.flatten())
-        sub_data_list.append(mo.coef_2.flatten())
-        data.append(np.concatenate(sub_data_list), axis=0)
+        sub_data_list.append(mo.coef_1.T)
+        sub_data_list.append(mo.coef_2.T)
+        data.append(np.concatenate(sub_data_list, axis=0))
 
-    cond_vec = np.arrange(len(mo.coef_1.flatten()))
+    cond_vec = np.arange(mo.coef_1.T.shape[0])
     cond_vec = np.concatenate([cond_vec, cond_vec])
 
-    part_vec = np.array([1] * len(mo.coef_1.flatten()) + [2] * len(mo.coef_2.flatten()))
+    part_vec = np.array([1] * mo.coef_1.T.shape[0] + [2] * mo.coef_2.T.shape[0])
 
     # Run the reliability function
-    vars = frel.decompose_subj_group(data, cond_vec, part_vec,
+    vars = frel.decompose_subj_group(np.array(data), cond_vec, part_vec,
                          separate='none',
                          subtract_mean=False)
 
@@ -64,6 +65,20 @@ if __name__ == "__main__":
     logalpha = 6
 
     ds_var_decom_df = new_var_decom(dataset_name, logalpha)
-    vars = rel_var_decom(dataset_name, logalpha)
+    vars = rel_var_decom(dataset_name, logalpha)[0]
+    new_vars = [
+        (ds_var_decom_df['sc']**2 * ds_var_decom_df['v_g']).mean(),
+        (ds_var_decom_df['sc']**2 * ds_var_decom_df['v_s']).mean(),
+        (ds_var_decom_df['sc']**2 * ds_var_decom_df['v_m']).mean()
+    ]
     
-    print('done')
+    print(f'\nNew Variance decomposition components: {[f"{v:.2e}" for v in new_vars]}')
+    print(f'Reliability Variance components: {[f"{v:.2e}" for v in vars]}')
+
+    correlation = np.corrcoef(vars, new_vars)[0, 1]
+    print(f'Correlation: {correlation}')
+
+    new_vars_percentage = [v / sum(new_vars) * 100 for v in new_vars]
+    vars_percentage = [v / sum(vars) * 100 for v in vars]
+    print(f'\nNew Variance decomposition percentages: {[f"{v:.2f}%" for v in new_vars_percentage]}')
+    print(f'Reliability Variance percentages: {[f"{v:.2f}%" for v in vars_percentage]}')
